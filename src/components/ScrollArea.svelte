d97b7f31c8b1040bac4a4879ab37e73261e0aa82
BruhaBruh <drugsho.jaker@gmail.com>
2022-09-15 03:38:16 +0500

    add scrollarea

---
+++ src/components/ScrollArea.svelte
@@ -0,0 +1,254 @@
+<script lang="ts" context="module">
+  import { onDestroy } from 'svelte';
+  import { tweened } from 'svelte/motion';
+  import { fade } from 'svelte/transition';
+  import { buildClass, debounce } from '../core/index';
+
+  type ScrollType = 'auto' | 'always' | 'scroll' | 'hover' | 'never';
+</script>
+
+<script lang="ts">
+  export let type: ScrollType = 'scroll';
+  export let typeY: ScrollType | undefined = undefined;
+  export let typeX: ScrollType | undefined = 'never';
+  export let scrollbarSize = 8;
+  export let scrollHideDelay = 1000;
+  export let offsetX = false;
+  export let offsetY = false;
+
+  const { start: startDebounceX, destroy: destroyX } =
+    debounce(scrollHideDelay);
+  const { start: startDebounceY, destroy: destroyY } =
+    debounce(scrollHideDelay);
+
+  onDestroy(() => {
+    destroyX();
+    destroyY();
+  });
+
+  $: finalTypeX = typeX ? typeX : type;
+  $: finalTypeY = typeY ? typeY : type;
+
+  let thumbXSize = 0;
+  let thumbXPosition = tweened(0, { duration: 150 });
+  let thumbYSize = 0;
+  let thumbYPosition = tweened(0, { duration: 150 });
+  let showX = false;
+  let showY = false;
+
+  let scrollable: HTMLDivElement;
+
+  const updateThumb = () => {
+    const height = scrollable.clientHeight;
+    const contentHeight = scrollable.scrollHeight;
+    thumbYSize = (height / contentHeight) * 100;
+
+    const width = scrollable.clientWidth;
+    const contentWidth = scrollable.scrollWidth;
+    thumbXSize = (width / contentWidth) * 100;
+  };
+
+  const handleScroll = (event: any) => {
+    updateThumb();
+
+    thumbXPosition.set(
+      (scrollable.scrollLeft /
+        ((scrollable as any).scrollLeftMax + scrollable.clientWidth)) *
+        100 ?? 0,
+    );
+    thumbYPosition.set(
+      (scrollable.scrollTop /
+        ((scrollable as any).scrollTopMax + scrollable.clientHeight)) *
+        100 ?? 0,
+    );
+
+    if (finalTypeX === 'scroll') {
+      showX = true;
+      startDebounceX(() => (showX = false));
+    }
+    if (finalTypeY === 'scroll') {
+      showY = true;
+      startDebounceY(() => (showY = false));
+    }
+  };
+
+  const handleMouseOver = () => {
+    if (finalTypeX === 'hover') {
+      showX = true;
+      startDebounceX(() => (showX = false));
+    }
+    if (finalTypeY === 'hover') {
+      showY = true;
+      startDebounceY(() => (showY = false));
+    }
+  };
+
+  const handleMouseDownY = (event: MouseEvent) => {
+    event.preventDefault();
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+    function onMouseMove(e: MouseEvent) {
+      const bouning = scrollable.getBoundingClientRect();
+      const offset = e.y - bouning.top;
+      const maxScroll = (scrollable as any).scrollTopMax;
+      scrollable.scrollTo({
+        left: 0,
+        top: (maxScroll * offset) / scrollable.clientHeight,
+      });
+    }
+
+    function onMouseUp() {
+      document.removeEventListener('mouseup', onMouseUp);
+      document.removeEventListener('mousemove', onMouseMove);
+    }
+  };
+
+  const handleMouseDownX = (event: MouseEvent) => {
+    event.preventDefault();
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+    function onMouseMove(e: MouseEvent) {
+      const bouning = scrollable.getBoundingClientRect();
+      const offset = e.x - bouning.left;
+      const maxScroll = (scrollable as any).scrollLeftMax;
+      scrollable.scrollTo({
+        left: (maxScroll * offset) / scrollable.clientWidth,
+        top: 0,
+      });
+    }
+
+    function onMouseUp() {
+      document.removeEventListener('mouseup', onMouseUp);
+      document.removeEventListener('mousemove', onMouseMove);
+    }
+  };
+
+  $: {
+    if (scrollable) updateThumb();
+  }
+
+  $: {
+    if (finalTypeX === 'auto') {
+      showX = thumbXSize < 100;
+    } else if (finalTypeX === 'always') {
+      showX = true;
+    } else if (finalTypeX === 'scroll') {
+      showX = false;
+    } else if (finalTypeX === 'hover') {
+      showX = false;
+    } else {
+      showX = false;
+    }
+  }
+
+  $: {
+    if (finalTypeY === 'auto') {
+      showY = thumbYSize < 100;
+    } else if (finalTypeY === 'always') {
+      showY = true;
+    } else if (finalTypeY === 'scroll') {
+      showY = false;
+    } else if (finalTypeY === 'hover') {
+      showY = false;
+    } else {
+      showY = false;
+    }
+  }
+</script>
+
+<svelte:window on:resize={updateThumb} />
+
+<div {...$$restProps} class={buildClass('scroll-area', $$restProps.class)}>
+  <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+  <div
+    class={buildClass(
+      'scroll-area__inner',
+      offsetX && 'scroll-area__inner-offset-x',
+      offsetY && 'scroll-area__inner-offset-y',
+    )}
+    on:scroll={handleScroll}
+    on:mouseover={handleMouseOver}
+    bind:this={scrollable}
+    style:--scrollbar-width={`${scrollbarSize}px`}
+  >
+    <div style="min-width: 100%; display: table;">
+      <slot />
+    </div>
+  </div>
+  {#if showX}
+    <div
+      class={buildClass(
+        'scroll-area__scrollbar',
+        'scroll-area__scrollbar-horizontal',
+      )}
+      style:--scrollbar-width={`${scrollbarSize}px`}
+      style:--scrollbar-thumb-position={`${$thumbXPosition}%`}
+      style:--scrollbar-thumb-size={`${thumbXSize}%`}
+      draggable
+      on:mousedown={handleMouseDownX}
+      on:dragstart={() => {}}
+      transition:fade={{ duration: 150 }}
+    />
+  {/if}
+  {#if showY}
+    <div
+      class={buildClass(
+        'scroll-area__scrollbar',
+        'scroll-area__scrollbar-vertical',
+      )}
+      style:--scrollbar-width={`${scrollbarSize}px`}
+      style:--scrollbar-thumb-position={`${$thumbYPosition}%`}
+      style:--scrollbar-thumb-size={`${thumbYSize}%`}
+      on:mousedown={handleMouseDownY}
+      on:dragstart={() => {}}
+      transition:fade={{ duration: 150 }}
+    />
+  {/if}
+</div>
+
+<style lang="postcss">
+  .scroll-area {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .scroll-area__inner {
+    overflow: scroll;
+    scrollbar-width: none;
+    width: 100%;
+    height: 100%;
+  }
+
+  .scroll-area__inner-offset-x > :global(* > *) {
+    padding-bottom: calc(var(--scrollbar-width) + 0.25rem);
+  }
+
+  .scroll-area__inner-offset-y > :global(* > *) {
+    padding-right: calc(var(--scrollbar-width) + 0.25rem);
+  }
+
+  .scroll-area__scrollbar {
+    position: absolute;
+    background-color: var(--theme-secondary-default);
+    border-radius: 9999px;
+    cursor: pointer;
+  }
+
+  .scroll-area__scrollbar-horizontal {
+    width: var(--scrollbar-thumb-size);
+    height: var(--scrollbar-width);
+    left: var(--scrollbar-thumb-position);
+    bottom: 0;
+  }
+
+  .scroll-area__scrollbar-vertical {
+    height: var(--scrollbar-thumb-size);
+    width: var(--scrollbar-width);
+    top: var(--scrollbar-thumb-position);
+    right: 0;
+  }
+</style>
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,5 @@
 export { default as buildClass } from './buildClass';
 export { default as buildStyle } from './buildStyle';
+export { default as debounce } from './debounce';
 export { default as ripple } from './ripple';
 export { default as theme } from './theme';
