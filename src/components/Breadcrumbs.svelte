149588d2dd424b54180e3813fdeb4f0fe4719a57
BruhaBruh <drugsho.jaker@gmail.com>
2022-09-19 13:22:57 +0500

    add breadcrumbs

---
+++ src/components/Breadcrumbs.svelte
@@ -0,0 +1,48 @@
+<script lang="ts" context="module">
+  import { buildClass } from '../core';
+</script>
+
+<script lang="ts">
+  export let separator = '-';
+  export let spacing = 16;
+</script>
+
+<div
+  {...$$restProps}
+  class={buildClass('breadcrumbs', $$restProps.class)}
+  style:--separator={`'${separator}'`}
+  style:--spacing={`${spacing}px`}
+>
+  <slot />
+</div>
+
+<style lang="postcss">
+  .breadcrumbs {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  .breadcrumbs > :global(*) {
+    position: relative;
+    margin-right: var(--spacing);
+  }
+
+  .breadcrumbs > :global(*::after) {
+    content: var(--separator);
+    position: absolute;
+    top: 50%;
+    left: calc(100% + calc(var(--spacing) / 2));
+    transform: translateX(-50%) translateY(-50%);
+    color: var(--theme-text-default);
+    pointer-events: none;
+  }
+
+  .breadcrumbs > :global(*:last-child) {
+    margin-right: inherit;
+  }
+
+  .breadcrumbs > :global(*:last-child::after) {
+    display: none;
+  }
+</style>
---
+++ src/stories/Breadcrumbs/Breadcrumbs.story.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" context="module">
+  import { Variant } from '@vitebook/client';
+  import Breadcrumbs from '../../components/Breadcrumbs.svelte';
+  import StoryWrapper from '../StoryWrapper.svelte';
+</script>
+
+<script lang="ts">
+</script>
+
+<StoryWrapper>
+  <Variant name="-">
+    <Breadcrumbs>
+      <a href="/">Ссылка 1</a>
+      <a href="/">Ссылка 2</a>
+      <a href="/">Ссылка 3</a>
+      <a href="/">Ссылка 4</a>
+    </Breadcrumbs>
+  </Variant>
+  <Variant name=">">
+    <Breadcrumbs separator=">" spacing={40}>
+      <a href="/">Ссылка 1</a>
+      <a href="/">Ссылка 2</a>
+      <a href="/">Ссылка 3</a>
+      <a href="/">Ссылка 4</a>
+    </Breadcrumbs>
+  </Variant>
+</StoryWrapper>
