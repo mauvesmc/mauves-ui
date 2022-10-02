2f92bdbc3f13efae451207ac9c344b7f9b0892cd
BruhaBruh <drugsho.jaker@gmail.com>
2022-10-02 19:40:53 +0500

    improve modal

--- src/lib/ui/Modal.svelte
+++ src/lib/ui/Modal.svelte
@@ -3,9 +3,15 @@
 </script>
 
 <script lang="ts">
+	export let title: string;
+	export let description = '';
 </script>
 
 <div {...$$restProps} class={buildClass('modal', $$restProps.class)}>
+	<h4 class={buildClass('modal__title', !description && 'modal__padding')}>{title}</h4>
+	{#if description}
+		<p class="modal__description modal__padding typography-b2">{description}</p>
+	{/if}
 	<slot />
 </div>
 
@@ -17,5 +23,20 @@
 		background-color: var(--theme-base-1);
 		color: var(--theme-text-default);
 		overscroll-behavior-y: contain;
+	}
+
+	.modal__title {
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+
+	.modal__description {
+		color: var(--theme-text-hover);
+		margin-top: 0.5rem;
+	}
+
+	.modal__padding {
+		margin-bottom: 1rem;
 	}
 </style>
