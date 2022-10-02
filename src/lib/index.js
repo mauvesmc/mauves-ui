4230863d2df8cecc2a31fc369750c604d2b19ada
BruhaBruh <drugsho.jaker@gmail.com>
2022-10-02 21:57:54 +0500

    add TextField

--- src/lib/index.js
+++ src/lib/index.js
@@ -33,5 +33,6 @@ export { default as Stepper } from './ui/Stepper.svelte';
 export { default as Switch } from './ui/Switch.svelte';
 export { default as Tabs } from './ui/Tabs.svelte';
 export { default as TextArea } from './ui/TextArea.svelte';
+export { default as TextField } from './ui/TextField.svelte';
 export { default as Time } from './ui/Time.svelte';
 export { default as getStyleElementFromTheme } from './utils/getStyleElementFromTheme';
---
+++ src/lib/ui/TextField.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" context="module">
+	import buildClass from '$lib/core/buildClass';
+	import randomId from '$lib/core/randomId';
+	import { slide } from 'svelte/transition';
+</script>
+
+<script lang="ts">
+	export let title = '';
+	export let error = '';
+	export let id = randomId();
+</script>
+
+<div {...$$restProps} class={buildClass('text-field', $$restProps.class)}>
+	{#if title}
+		<label class="text-field__title typography-b2" for={id}>{title}</label>
+	{/if}
+	<slot {id} />
+	{#if error}
+		<p transition:slide={{ duration: 150 }} class="text-field__error typography-ht">{error}</p>
+	{/if}
+</div>
+
+<style lang="postcss">
+	.text-field__title {
+		margin-bottom: 0.25rem;
+		display: block;
+	}
+
+	.text-field__error {
+		margin-top: 0.125rem;
+		color: var(--theme-red-default);
+	}
+</style>
