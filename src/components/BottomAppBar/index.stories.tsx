420821a1e1774f46bc66a4536176d96f8508e056
BruhaBruh <drugsho.jaker@gmail.com>
2022-11-16 14:17:12 +0500

    docs: change icons in bottom app bar

--- src/components/BottomAppBar/index.stories.tsx
+++ src/components/BottomAppBar/index.stories.tsx
@@ -41,7 +41,7 @@ const Template = ((args: BottomAppBarProps) => (
     <IconButton variant="standard">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="icon icon-tabler icon-tabler-search"
+        class="icon icon-tabler icon-tabler-circle"
         width="24"
         height="24"
         viewBox="0 0 24 24"
@@ -52,14 +52,13 @@ const Template = ((args: BottomAppBarProps) => (
         stroke-linejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <circle cx="10" cy="10" r="7" />
-        <line x1="21" y1="21" x2="15" y2="15" />
+        <circle cx="12" cy="12" r="9" />
       </svg>
     </IconButton>
     <IconButton variant="standard">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="icon icon-tabler icon-tabler-trash"
+        class="icon icon-tabler icon-tabler-box"
         width="24"
         height="24"
         viewBox="0 0 24 24"
@@ -70,17 +69,16 @@ const Template = ((args: BottomAppBarProps) => (
         stroke-linejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <line x1="4" y1="7" x2="20" y2="7" />
-        <line x1="10" y1="11" x2="10" y2="17" />
-        <line x1="14" y1="11" x2="14" y2="17" />
-        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+        <polyline points="12 3 20 7.5 20 16.5 12 21 4 16.5 4 7.5 12 3" />
+        <line x1="12" y1="12" x2="20" y2="7.5" />
+        <line x1="12" y1="12" x2="12" y2="21" />
+        <line x1="12" y1="12" x2="4" y2="7.5" />
       </svg>
     </IconButton>
     <IconButton variant="standard">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="icon icon-tabler icon-tabler-archive"
+        class="icon icon-tabler icon-tabler-square"
         width="24"
         height="24"
         viewBox="0 0 24 24"
@@ -91,15 +89,13 @@ const Template = ((args: BottomAppBarProps) => (
         stroke-linejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <rect x="3" y="4" width="18" height="4" rx="2" />
-        <path d="M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-10" />
-        <line x1="10" y1="12" x2="14" y2="12" />
+        <rect x="4" y="4" width="16" height="16" rx="2" />
       </svg>
     </IconButton>
     <IconButton variant="standard">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="icon icon-tabler icon-tabler-photo"
+        class="icon icon-tabler icon-tabler-cookie"
         width="24"
         height="24"
         viewBox="0 0 24 24"
@@ -110,10 +106,12 @@ const Template = ((args: BottomAppBarProps) => (
         stroke-linejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <line x1="15" y1="8" x2="15.01" y2="8" />
-        <rect x="4" y="4" width="16" height="16" rx="3" />
-        <path d="M4 15l4 -4a3 5 0 0 1 3 0l5 5" />
-        <path d="M14 14l1 -1a3 5 0 0 1 3 0l2 2" />
+        <path d="M8 13v.01" />
+        <path d="M12 17v.01" />
+        <path d="M12 12v.01" />
+        <path d="M16 14v.01" />
+        <path d="M11 8v.01" />
+        <path d="M13.148 3.476l2.667 1.104a4 4 0 0 0 4.656 6.14l.053 .132a3 3 0 0 1 0 2.296c-.497 .786 -.838 1.404 -1.024 1.852c-.189 .456 -.409 1.194 -.66 2.216a3 3 0 0 1 -1.624 1.623c-1.048 .263 -1.787 .483 -2.216 .661c-.475 .197 -1.092 .538 -1.852 1.024a3 3 0 0 1 -2.296 0c-.802 -.503 -1.419 -.844 -1.852 -1.024c-.471 -.195 -1.21 -.415 -2.216 -.66a3 3 0 0 1 -1.623 -1.624c-.265 -1.052 -.485 -1.79 -.661 -2.216c-.198 -.479 -.54 -1.096 -1.024 -1.852a3 3 0 0 1 0 -2.296c.48 -.744 .82 -1.361 1.024 -1.852c.171 -.413 .391 -1.152 .66 -2.216a3 3 0 0 1 1.624 -1.623c1.032 -.256 1.77 -.476 2.216 -.661c.458 -.19 1.075 -.531 1.852 -1.024a3 3 0 0 1 2.296 0z" />
       </svg>
     </IconButton>
   </BottomAppBar>
